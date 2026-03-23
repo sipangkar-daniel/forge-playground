c2a6ac7b61dcaf9e6b06729ada427b696d99f630
Daniel Sipangkar <daniel222mu@gmail.com>
2026-03-24 00:14:35 +0700

    new branch only for learn typescript

--- learn-golang/01-golangdasar/03-datatype/string.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Daniel Sipangkar")
-	fmt.Println("Daniel Sipangkar"[0])
-}
--- learn-golang/go.mod
+++
@@ -1,3 +0,0 @@
-module learn-golang
-
-go 1.25.5
